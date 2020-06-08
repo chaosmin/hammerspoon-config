ad6249b7aabf2a9d78978bf8978869eee636aa79
Romani min <minchao1029@hotmail.com>
2020-06-08 13:19:30 +0800

    init repo

---
+++ init.lua
@@ -0,0 +1,13 @@
+print('==================================================')
+require "headphone.headphone"
+require "hotkey.hotkey"
+require "ime.ime"
+require "reload.reload"
+require "usb.usb"
+require "wifi.wifi"
+require "window.window"
+require "clipboard.clipboard"
+require "statuslets.statuslets"
+require "volume.volume"
+require "weather.weather"
+require "speaker.speaker"
